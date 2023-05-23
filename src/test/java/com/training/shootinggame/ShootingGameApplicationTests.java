f8d75da6dad1a7c0dffdf909ae7f8d15b4693341
k-chandravir <kumar.chandravir@m2pfintech.com>
2023-05-23 17:42:40 +0530

    Initial Commit

---
+++ src/test/java/com/training/shootinggame/ShootingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.shootinggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShootingGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/training/shootinggame/ShootingServiceTest.java
@@ -0,0 +1,4 @@
+package com.training.shootinggame;
+
+public class ShootingServiceTest {
+}
